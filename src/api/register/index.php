80058f503e8928b7c297e28aef7c73bb6075f78e
cosmin-bilga <cosmin.bilga@laplateforme.io>
2025-11-25 08:52:35 +0100

    API routes + docs

--- src/api/register/index.php
+++ src/api/register/index.php
@@ -17,13 +17,15 @@ require_once "../connection.php";
 
 header("Content-Type: application/json; charset=UTF-8");
 
-//var_dump($_SERVER['REQUEST_METHOD']);
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
         $requestData = $_POST;
         customer_register($requestData);
         break;
+    case 'OPTIONS':
+        echo json_encode(["message" => "OPTIONS"]);
+        http_response_code(200);
     default:
         echo json_encode(["message" => "Invalid request"]);
         http_response_code(400); // BAD REQUEST
@@ -33,10 +35,45 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
 function validate_name(string $name): string
 {
-    if (isset($name) && strlen($name) > 2)
-        return "";
-    else
-        return "Invalid name.";
+    if (!isset($name))
+        return "Name is missing.";
+    else if (ctype_alpha($name) === false)
+        return "Name can only contain letters.";
+    else if (strlen($name) <= 2)
+        return "Name too short. (min 2 characters)";
+    return "";
+}
+
+function validate_firstname(string $name): string
+{
+    if (!isset($name))
+        return "Firstname is missing.";
+    else if (ctype_alpha($name) === false)
+        return "Firstname can only contain letters.";
+    else if (strlen($name) <= 2)
+        return "Firstname too short. (min 2 characters)";
+    return "";
+}
+
+function validate_email(string $email): string
+{
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        return "Email is not valid";
+    return "";
+}
+
+function validate_phone_number(string $phone_number): string
+{
+    if (!preg_match("/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}/", $phone_number))
+        return "Invalid phone number";
+    return "";
+}
+
+function validate_sex(string $sex): string
+{
+    if ($sex !== "M" && $sex !== "F")
+        return "Sex must be M or F";
+    return "";
 }
 
 function validate_input(array $requestData): array
@@ -46,13 +83,22 @@ function validate_input(array $requestData): array
         array_push($errors, "Error: name is not set");
     elseif (($err = validate_name($requestData["name"])) != "")
         array_push($errors, $err);
-
-    /*  string $firstname = "",
-    string $email = "",
-    string $phone_number = "",
-    string $address = "",
-    string $sex = "",
-    string $additional_info = "" */
+    if (!isset($requestData["firstname"]))
+        array_push($errors, "Error: firstname is not set");
+    elseif (($err = validate_firstname($requestData["firstname"])) != "")
+        array_push($errors, $err);
+    if (!isset($requestData["email"]))
+        array_push($errors, "Error: email is not set");
+    elseif (($err = validate_email($requestData["email"])) != "")
+        array_push($errors, $err);
+    if (!isset($requestData["phone_number"]))
+        array_push($errors, "Error: phone number is not set");
+    elseif (($err = validate_phone_number($requestData["phone_number"])) != "")
+        array_push($errors, $err);
+    if (!isset($requestData["sex"]))
+        array_push($errors, "Error: sex is not set");
+    elseif (($err = validate_sex($requestData["sex"])) != "")
+        array_push($errors, $err);
     $requestData["errors"] = $errors;
     return $requestData;
 }
